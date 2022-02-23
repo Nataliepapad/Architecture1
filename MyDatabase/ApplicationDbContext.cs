cfbc325f776de559cf3efb93a934dc0b12a49d1a
Nataliepapad <papadnatalia@gmail.com>
2022-02-23 21:34:43 +0200

    SeedingCourses

--- MyDatabase/ApplicationDbContext.cs
+++ MyDatabase/ApplicationDbContext.cs
@@ -26,5 +26,6 @@ namespace MyDatabase
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Project> Projects { get; set; }
+        public DbSet<Course> Courses { get; set; }
     }
 }
