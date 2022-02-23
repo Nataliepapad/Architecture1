7be15d84575da68311d24be55843b38ebd235f14
sofianna12 <sofianna02@gmail.com>
2022-02-23 22:46:16 +0200

    Assignments Repositories

---
+++ PersistenceGeneric/Repositories/AssignmentsRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.School;
+using MyDatabase;
+using PersistenceGeneric.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistenceGeneric.Repositories
+{
+    public class AssignmentsRepository : GenericRepository<Assignment>, IAssignmetnRepository
+    {
+        public AssignmentsRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Assignment> GetAssignmentsOrderBy()
+        {
+            return Context.Assignments.OrderBy(x => x.Name).ToList();
+        }
+
+        
+    }
+}
